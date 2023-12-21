931c22779b3558635d4664afe163c4c5fb237b97
krutarth78 <145321741+krutarth78@users.noreply.github.com>
2023-12-20 19:51:49 -0500

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,17 +1,40 @@
-import React from "react";
-import { 
-    Box, 
-    CssBaseline,
-    AppBar,
-    Toolbar,
-    Typography,
-    Button
-} from "@mui/material";
+import React from 'react';
+import { Typography, Button, Paper, Box, Container } from '@mui/material';
+import { styled } from '@mui/system';
+import Image from '../assets/HomePage.png';
+import { NavLink } from "react-router-dom";
 
-export function HomePage(){
-    return (
-        <Box display={'flex'} justifyContent={'center'} alignContent={'center'}> 
-            <Typography variant="h4">Home Page</Typography>
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(4),
+  margin: theme.spacing(2),
+  textAlign: 'center',
+}));
+
+export const HomePage = () => {
+  return (
+    <Container maxWidth="lg">
+      <Box display="flex" flexDirection="row" justifyContent="space-between" alignItems="center" p={4}>
+        <Box flex={1} p={2}>
+          <Typography variant="h3" component="h1" gutterBottom>
+            <b>LAUNCH YOUR CAREER QUEST WITH CLARITY</b>
+          </Typography>
+          <Typography variant="h5" component="p" gutterBottom>
+            Plan, Track, Achieve!
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            Your career journey, mapped and mastered. With our intuitive tracking, turn every application into a stepping stone towards success.
+          </Typography>
+            <br />
+          <Button variant="contained" color="primary" size="large" component={NavLink} to="/signup">
+            
+            Get Started
+          </Button>
         </Box>
-    )
-}
+        <Box flex={1} p={2}>
+            <img src={Image} alt="Application Preview" style={{ width: '100%', height: 'auto', border: '2px solid' }} />
+          
+        </Box>
+      </Box>
+    </Container>
+  );
+};
